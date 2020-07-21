43a1a7c58c69d74e5b0b5ee512ed2e3dcbc7cc56
carpenter-zhen <zhenjing93@163.com>
2020-07-21 09:47:10 +0800

    项目创建

---
+++ src/main/java/com/zhenjing/mymall/model/User.java
@@ -0,0 +1,12 @@
+package com.zhenjing.mymall.model;
+
+/**
+ * @author ：甄竞
+ * @date ：Created By 2020/7/21 9:32
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class User {
+    private String User;
+}
